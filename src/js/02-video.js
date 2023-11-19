d0a576abbdaebdb3d44199fe015ed76e557734ee
KZapal <kamil.zapal@o2.pl>
2023-11-19 19:28:21 +0100

    finish?

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo(iframe);
+
+player.on('loaded', () => {
+  const currentTime = localStorage.getItem(`videoplayer-current-time`);
+  player.setCurrentTime(currentTime);
+});
+
+player.on(
+  'timeupdate',
+  throttle(event => {
+    localStorage.setItem(`videoplayer-current-time`, event.seconds);
+  }, 1000)
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(`.feedback-form`);
+
+form.addEventListener(
+  `input`,
+  throttle(event => {
+    const dane = {
+      email: form.email.value,
+      message: form.message.value,
+    };
+    localStorage.setItem(`feedback-form-state`, JSON.stringify(dane));
+  }, 500)
+);
+
+form.addEventListener(`submit`, event => {
+  event.preventDefault();
+  if (form.message.value && form.email.value) {
+    form.message.value = '';
+    form.email.value = '';
+    console.log(JSON.parse(localStorage.getItem(`feedback-form-state`)));
+    localStorage.clear();
+  }
+});
+
+let obj = JSON.parse(localStorage.getItem(`feedback-form-state`));
+form.email.textContent = obj.email;
+form.message.textContent = obj.message;
